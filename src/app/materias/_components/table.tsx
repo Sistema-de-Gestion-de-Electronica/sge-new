cf2caef68a6d855ee4450764669ac22a311233cf
fmarsico8 <102746644+fmarsico8@users.noreply.github.com>
2025-09-13 19:55:42 -0300

    feat: add button materia detalle

--- src/app/materias/_components/table.tsx
+++ src/app/materias/_components/table.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { DataTable } from "@/components/ui";
+import { Button, DataTable } from "@/components/ui";
 import { api, type RouterOutputs } from "@/trpc/react";
 import { getColumns } from "./columns";
 import RemoveMateriaModal from "./remove-materia";
@@ -11,16 +11,14 @@ import { TienePermiso } from "@/app/_components/permisos/tienePermiso";
 import { SgeNombre } from "@/generated/prisma";
 import { useRouter, useSearchParams } from "next/navigation";
 import { MATERIA_ROUTE } from "@/shared/server-routes";
+import ButtonMateriaDetalle from "./button-materia-detalle"; 
+
 
 type MateriaData = RouterOutputs["materia"]["getAll"];
 
 type MateriasTableProps = {
   data: MateriaData;
 };
-
-function handleRowClick(id: Number){
-  
-}
 
 export const MateriasTable = ({ data }: MateriasTableProps) => {
   const columns = getColumns();
@@ -39,7 +37,7 @@ export const MateriasTable = ({ data }: MateriasTableProps) => {
     refreshGetAll();
   };
 
-  function handleRowClick(id: number) {
+  function goToDetail(id: number) {
     const next = new URLSearchParams(sp.toString());
     next.set("materiaId", String(id));
     router.push(`${MATERIA_ROUTE.href}?${next.toString()}`);
@@ -61,13 +59,13 @@ export const MateriasTable = ({ data }: MateriasTableProps) => {
                     <EditMateriaModal materiaId={original.id.toString()} />
                   </>
                 </TienePermiso>
+                <ButtonMateriaDetalle id={original.id} />
               </>
             );
           },
         }}
         grouping={grouping}
         setGrouping={setGrouping}
-        onRowClick={(row) => handleRowClick(row.original.id)}
       />
     </>
   );

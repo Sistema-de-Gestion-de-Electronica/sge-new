4b1bcb8691ce1b5f5fe687d9ae37e9069b6652a7
Chiara Beretta <chiarasofiaberetta@gmail.com>
2025-10-02 20:42:19 -0300

    feat: email input added

--- src/server/api/services/mails/emailVotacionAbierta.service.ts
+++ src/server/api/services/mails/emailVotacionAbierta.service.ts
@@ -3,22 +3,20 @@ import { getAllConsejeros } from "../../repositories/actas/actas.repository";
 import { sendEmail } from "./email";
 import { ACTAS_ROUTE } from "@/shared/server-routes";
 
-export const enviarMailNuevaVotacionAbiertaProcedure = async (ctx: { db: PrismaClient }, acta: Acta) => {
+export const enviarMailNuevaVotacionAbiertaProcedure = async (ctx: { db: PrismaClient }, acta: Acta, cuerpoMail: String) => {
   const consejeros = getAllConsejeros(ctx);
 
   (await consejeros).forEach(async consejero => {
+      if(!consejero.email) return;
       await sendEmail(ctx, {
         asunto: "Nuevo Acta de consejo disponible para votación",
-        to: consejero.email ?? "",
+        to: consejero.email,
         usuario: {
           nombre: consejero.nombre ?? "Usuario",
           apellido: consejero.apellido ?? "",
         },
         textoMail: `
-          <p style="text-align: center;"><strong>¡Nueva votación disponible!</strong></p>
-          <p>Le informamos que se encuenta disponible el Acta con fecha <strong>${acta.fechaReunion}</strong> en el sistema de gestión académica SGE.</p>
-          <p>Por favor, inngrese al sistema para revisarla y emitir su voto correspondiente.</p>
-          <p>Agradecemos su participación y compromiso con las acividades del Consejo.</p>
+          <p style="text-align: center;"><strong>${cuerpoMail}</p>
         `,
         hipervinculo:
           ACTAS_ROUTE.href !== undefined ? String(ACTAS_ROUTE?.href) : "",
